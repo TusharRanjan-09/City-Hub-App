30b565b2599a6a5cb1012485d70810a08d75c0dc
TusharRanjan-09 <tushar.r@jeanmartin.com>
2021-02-09 18:14:37 +0530

    Updated

--- cityHub/src/components/sharedComponents/Carousel.js
+++ cityHub/src/components/sharedComponents/Carousel.js
@@ -1,18 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, TextInput,Dimensions,ScrollView, ImageBackground, Image } from 'react-native';
-
+import { useNavigation, useTheme } from '@react-navigation/native';
 import Category from '../sharedComponents/categoryCarousel';
 import OfferBanner from '../sharedComponents/offerBanner';
 import CardCarousel from '../sharedComponents/cardCarousel';
 import CardItems from '../sharedComponents/cardItems';
 import IconCategory from '../sharedComponents/IconCategory';
 import Footer from '../sharedComponents/Footer';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 export default function Carousel({navigation}) {
+    const {colors} = useTheme()
   return (
     <View style={styles.container}>
         <ScrollView>
-                <Text style={styles.title}>Categories</Text>
+                <Text style={{fontSize: 34,color: colors.txt,fontWeight: 'bold',paddingHorizontal: 20,paddingTop: 5,paddingBottom: 5}}>Categories</Text>
                 <ScrollView horizontal={true}>
                     <Category />
                     <Category />
@@ -20,28 +22,28 @@ export default function Carousel({navigation}) {
                     <Category />
                     <Category />
                 </ScrollView>
-                <Text style={styles.title}>Offers</Text>
+                <Text style={{fontSize: 34,color: colors.txt,fontWeight: 'bold',paddingHorizontal: 20,paddingTop: 5,paddingBottom: 5}}>Offers</Text>
                 <ScrollView horizontal={true}>
                     <OfferBanner />
                     <OfferBanner />
                     <OfferBanner />
                     <OfferBanner />
                 </ScrollView>
-                <Text style={styles.title}>Our Collections</Text>
+                <Text style={{fontSize: 34,color: colors.txt,fontWeight: 'bold',paddingHorizontal: 20,paddingTop: 5,paddingBottom: 5}}>Our Collections</Text>
                 <ScrollView horizontal={true}>
                     <CardCarousel/>
                     <CardCarousel/>
                     <CardCarousel/>
                     <CardCarousel/>
                 </ScrollView>
-                <Text style={styles.title}>Card Items</Text>
+                <Text style={{fontSize: 34,color: colors.txt,fontWeight: 'bold',paddingHorizontal: 20,paddingTop: 5,paddingBottom: 5}}>Card Items</Text>
                 <ScrollView horizontal={true}>
                     <CardItems/>
                     <CardItems/>
                     <CardItems/>
                     <CardItems/>
                 </ScrollView>
-                <Text style={styles.title}>Icon Categories</Text>
+                <Text style={{fontSize: 34,color: colors.txt,fontWeight: 'bold',paddingHorizontal: 20,paddingTop: 5,paddingBottom: 5}}>Icon Categories</Text>
                 <ScrollView horizontal={true}>
                   <IconCategory/>
                   <IconCategory/>
