93a6a7eeb26b520bc563506173d4b5df8f0f872c
TusharRanjan-09 <tushar.r@jeanmartin.com>
2021-02-25 12:15:18 +0530

    Updated

--- cityHub/src/screens/Explore.js
+++ cityHub/src/screens/Explore.js
@@ -4,15 +4,28 @@ import { StyleSheet,SafeAreaView, Text, View, TouchableOpacity, TextInput, Image
 import Carousel from '../components/sharedComponents/Carousel';
 import Search from "../components/sharedComponents/Search";
 import { useNavigation, useTheme } from '@react-navigation/native';
+export const scaleSize = (size, width) => (width / 375) * size;
 export default function Explore() {
     const {colors} = useTheme()
-    const Width = Dimensions.get('screen').width
-    const Height = Dimensions.get('screen').height
     return (
-        <SafeAreaView style={{backgroundColor: colors.backGround, alignItems: 'center',width: Width,height: Height, paddingBottom:105}}>
+        <SafeAreaView style={[styles.container,{backgroundColor:colors.backGround}]}>
             <Search />
            <Carousel />
         </SafeAreaView>
     );
 }
-
+const {width, height} =  Dimensions.get('window')
+const buttonWidth = scaleSize(305, width)
+const buttonHeight = scaleSize(24, height)
+const inputWidth = scaleSize(305, width)
+const inputHeight = scaleSize(34, height)
+const styles = StyleSheet.create({
+  container: {
+    width: width,
+    height: height,
+    // backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent:'center',
+    paddingTop:35
+  },
+});
