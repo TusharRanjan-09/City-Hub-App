34e78da982da42fc41e67127b935e06494150668
TusharRanjan-09 <tushar.r@jeanmartin.com>
2021-02-25 12:49:10 +0530

    Updated

--- cityHub/src/components/sharedComponents/categoryCarousel.js
+++ cityHub/src/components/sharedComponents/categoryCarousel.js
@@ -4,14 +4,15 @@ import { StyleSheet, Text, View, TouchableOpacity, TextInput,Dimensions, ImageBa
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import { useNavigation, useTheme } from '@react-navigation/native';
+export const scaleSize = (size, width) => (width / 375) * size;
 export default function categoryCarousel() {
   const navigation = useNavigation();
   return (
     <View style={styles.container}>
         <TouchableOpacity onPress={()=> navigation.navigate("Hotels")}>
-        <View style={{backgroundColor: '#fc0',width: width-170, height: height-680, borderRadius: 20, flexDirection: 'row', alignItems: 'center'}}>
+        <View style={{backgroundColor: '#fc0',width: buttonWidth*.95, height: inputHeight*1.2, borderRadius: 20, flexDirection: 'row', alignItems: 'center',justifyContent:'center'}}>
          <Image source={{uri: 'https://images.unsplash.com/photo-1582719508461-905c673771fd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=925&q=80'}}
-         style={{width: width-300, height: height-700, borderRadius: 40, margin: 10}}
+         style={{width:inputHeight*1.0, height: inputHeight*1.0, borderRadius: inputHeight/2, margin:inputHeight*.1}}
          />
      <Text style={{fontSize: 29, color: 'white', fontWeight: 'bold'}}>Hotels</Text>
         </View>
@@ -19,13 +20,16 @@ export default function categoryCarousel() {
     </View>
   );
 }
-const width = Dimensions.get('screen').width
-const height = Dimensions.get('screen').height
+const {width, height} =  Dimensions.get('window')
+const buttonWidth = scaleSize(200, width)
+const buttonHeight = scaleSize(45, height)
+const inputWidth = scaleSize(340, width)
+const inputHeight = scaleSize(34, height)
 const styles = StyleSheet.create({
   container: {
-//    flex: 1,
-    // backgroundColor: '#fff',
-   margin: 10
+    width: width/1.8,
+    height:buttonHeight,
+    alignItems: 'center',
+    justifyContent:'center',
   },
- 
 });
